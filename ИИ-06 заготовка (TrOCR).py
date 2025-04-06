3484960d153505f2b40c248125bd1afd3deb10a4
Varvara Babichuk <babichuk.varvara@mail.ru>
2025-04-07 01:12:24 +0500

    Update ИИ-06 заготовка (TrOCR).py

--- ИИ-06 заготовка (TrOCR).py
+++ ИИ-06 заготовка (TrOCR).py
@@ -26,7 +26,7 @@ if result:
     captioner = pipeline(
         "image-to-text", 
         "microsoft/trocr-large-printed",
-        token=st.secrets["HUGGINGFACE_TOKEN"])
+        token=st.secrets["HUGGINGFACE_TOKEN"], max_new_tokens=100)
     text = captioner(img)
     st.write('Результаты распознавания:')
     st.write(text[0]["generated_text"])
