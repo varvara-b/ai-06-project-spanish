9b10e95febcff5ec19dcf3e8351b7f585a841ac3
Varvara Babichuk <babichuk.varvara@mail.ru>
2025-04-06 21:33:02 +0500

    Update ИИ-06 заготовка (TrOCR).py

--- ИИ-06 заготовка (TrOCR).py
+++ ИИ-06 заготовка (TrOCR).py
@@ -1,3 +1,4 @@
+import torch
 import io
 import streamlit as st
 
@@ -21,7 +22,7 @@ img = load_image()
 
 result = st.button('Распознать изображение')
 if result:
-    captioner = pipeline("image-to-text", "")  # в кавычках нужно указать название модели
+    captioner = pipeline("image-to-text", "microsoft/trocr-large-printed")
     text = captioner(img)
     st.write('Результаты распознавания:')
     st.write(text[0]["generated_text"])
