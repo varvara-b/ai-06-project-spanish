3c2c8cfe013093627f7b2790f4adcbde224ab9d5
Varvara Babichuk <babichuk.varvara@mail.ru>
2025-04-07 00:43:08 +0500

    Update ИИ-06 заготовка (TrOCR).py

--- ИИ-06 заготовка (TrOCR).py
+++ ИИ-06 заготовка (TrOCR).py
@@ -22,7 +22,7 @@ img = load_image()
 
 result = st.button('Распознать изображение')
 if result:
-    captioner = pipeline("image-to-text", "microsoft/trocr-large-printed")
+    captioner = pipeline("image-to-text", "microsoft/trocr-base-printed")
     text = captioner(img)
     st.write('Результаты распознавания:')
     st.write(text[0]["generated_text"])
